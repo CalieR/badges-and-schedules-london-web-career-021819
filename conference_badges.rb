fad6f5c36ac2c107187229c329ccb6fca7c4450c
Calie Rushton <calielrushton@gmail.com>
2019-01-02 13:58:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,5 +12,5 @@ def assign_rooms(speakers)
   # iterate over array, create message with index+1 as room number
   room_list = []
   speakers.each_with_index { |i, index| room_list << "Hello, #{i}! You'll be assigned to room #{index + 1}!"}
-  return room_list
+  room_list
 end
