879b72a380f7637ac399109ae3e1b51bc60a5700
Calie Rushton <calielrushton@gmail.com>
2019-01-02 13:55:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,8 +5,11 @@ end
 def batch_badge_creator(speakers)
   # return new array with badge messages
    speakers.collect { |x| badge_maker(x) }
+  # using collect negates need for creating and pushing to new array
 end
 
 def assign_rooms(speakers)
-  
+  # iterate over array, create message with index+1 as room number
+  room_list = []
+  speakers.each_with_index { |i, index| room_list << "Hello, #{i}! You'll be assigned to room #{index+1}!"}  
 end
